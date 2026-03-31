fc6a9c024710751ceb1baa8558548bbbff3486d5
IsabelaLemoss <bellahgodoy15@gmail.com>
2026-03-31 16:26:09 -0400

    adiciona funcao de agendamento de banho

--- cadastro_pets.py
+++ cadastro_pets.py
@@ -3,3 +3,9 @@ pets = []
 
 def cadastrar_pet(nome, raca, dono):
     pets.append({'nome': nome, 'raca': raca, 'dono': dono})
+    print(f'Banho agendado para {nome_pet} em {data} as {hora}')
+def agendar_banho(nome_pet, data, hora):
+    print("Banho agendado para " + nome_pet + " em " + data + " as " + hora)
+
+def listar_agendamentos():
+    print("Listando todos os agendamentos do dia...")
